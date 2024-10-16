a573a51380657d04a70dc68a2467d8e3ed13477f
RoshanRajcmd <roshanraj5121999@gmail.com>
2024-10-17 00:17:41 +0530

    Added connection for API running in different port

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import { useEffect } from 'react';
 import { LoginPage } from './components/LoginPage'
 
 function App() {
+  //Comment the below useEffect line if you are working only on the front end
+  //The below line will access the spring boot application running on the given port
+  useEffect(() => {
+    fetch('http://localhost:8080/api/v1/user/getUsers')
+      .then(response => response.json())
+      .then(result => console.log(result));
+  })
+
   return (
     <div className="App">
       <LoginPage />
