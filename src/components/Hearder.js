3978de96dd833078f6ae8739976602a8613c2f44
RoshanRajcmd <roshanraj5121999@gmail.com>
2024-11-23 19:58:38 +0530

    Refactor Header.js

--- src/components/Hearder.js
+++ src/components/Hearder.js
@@ -5,7 +5,7 @@ export function Header({ toggleModal, noOfBooks }) {
         <header class="flex flex-row justify-between items-center mt-12">
             <div class="flex flex-row items-center w-auto ms-auto me-auto overflow-hidden gap-4 ">
                 <h3>Books List {noOfBooks}</h3>
-                <button onClick={() => toggleModal(true)} class="flex text-white bg-blue-700 text-xs p-3 whitespace-normal rounded-lg shadow-sm transition-shadow">
+                <button onClick={() => toggleModal(true)} class="flex text-white bg-yellow-500 text-xs font-medium p-3 whitespace-normal rounded-3xl shadow-sm transition-shadow">
                     <img src=
                         "https://media.geeksforgeeks.org/wp-content/uploads/20240227164304/visible.png"
                         class="mr-2 w-4"></img>Add New Book
