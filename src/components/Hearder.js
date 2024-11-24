802a55a4d5ad34b60be3ce86156cf9c4653c57dc
RoshanRajcmd <roshanraj5121999@gmail.com>
2024-11-24 14:06:19 +0530

    Fixed User login cred validation and implemented font awesome as icons provider

--- src/components/Hearder.js
+++ src/components/Hearder.js
@@ -5,10 +5,8 @@ export function Header({ toggleModal, noOfBooks }) {
         <header class="flex flex-row justify-between items-center mt-12">
             <div class="flex flex-row items-center w-auto ms-auto me-auto overflow-hidden gap-4 ">
                 <h3>Books List {noOfBooks}</h3>
-                <button onClick={() => toggleModal(true)} class="flex text-white bg-yellow-500 text-xs font-medium p-3 whitespace-normal rounded-3xl shadow-sm transition-shadow">
-                    <img src=
-                        "https://media.geeksforgeeks.org/wp-content/uploads/20240227164304/visible.png"
-                        alt="" class="mr-2 w-4"></img>Add New Book
+                <button onClick={() => toggleModal(true)} class="flex items-center text-white bg-yellow-500 text-xs font-medium p-3 whitespace-normal rounded-3xl shadow-sm transition-shadow">
+                    <i class="fa fa-plus fa-lg mr-2" alt="Add New Book"></i>Add New Book
                 </button>
             </div>
         </header>
