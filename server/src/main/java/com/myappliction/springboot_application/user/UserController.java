fc43acee02966c26051c6c0be80b27a40bc16399
roshanrajcmd <roshanraj5121999@gmail.com>
2025-03-07 13:39:19 +0530

    Log4j2 configuration in server

--- server/src/main/java/com/myappliction/springboot_application/user/UserController.java
+++ server/src/main/java/com/myappliction/springboot_application/user/UserController.java
@@ -16,10 +16,8 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping(path="/myreads/api/v1/user")
+@RequestMapping(path = "/myreads/api/v1/user")
 public class UserController {
-
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private final UserService userService;
 
@@ -28,104 +26,105 @@ public class UserController {
         this.userService = userService;
     }
 
-    //API to get all the Users in Users table along with its details
-    //The CrossOrigin annotation is used when the API is getting access from other ports like when the front end of the
-    //application is trying to access the API from the request mapping path
+    // API to get all the Users in Users table along with its details
+    // The CrossOrigin annotation is used when the API is getting access from other
+    // ports like when the front end of the
+    // application is trying to access the API from the request mapping path
     @GetMapping
     @CrossOrigin
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    //Gives all the details of user by the given ID
+    // Gives all the details of user by the given ID
     @GetMapping(path = "/getUserDetails/{userId}")
-    public User getUserByID(@PathVariable("userId") String userId){
+    public User getUserByID(@PathVariable("userId") String userId) {
         return userService.getUser(userId);
     }
 
-    //Checks if given email id is already taken by someone
+    // Checks if given email id is already taken by someone
     @GetMapping(path = "/checkEmailExists/{email}")
-    public boolean isUserByEmailExist(@PathVariable("email") String email){
+    public boolean isUserByEmailExist(@PathVariable("email") String email) {
         return userService.isUserByEmailExist(email);
     }
 
-    //Validates the user credentials given in Database
+    // Validates the user credentials given in Database
     @GetMapping(path = "/validateUser")
-    public String validateUserCred(@RequestParam(value="email", defaultValue = "") String email,
-                                    @RequestParam(value="password", defaultValue = "") String password){
-        return userService.validateUserCred(email,password);
+    public String validateUserCred(@RequestParam(value = "email", defaultValue = "") String email,
+            @RequestParam(value = "password", defaultValue = "") String password) {
+        return userService.validateUserCred(email, password);
     }
 
     @GetMapping(path = "/checkUsernameExists/{username}")
-    public boolean isUserByUsernameExist(@PathVariable(value="username") String username){
+    public boolean isUserByUsernameExist(@PathVariable(value = "username") String username) {
         return userService.isUserByUsernameExist(username);
     }
 
     @GetMapping(path = "/checkOldPasswordValid")
-    public boolean isOldPasswordValid(@RequestParam(value="userId", defaultValue = "") String userId,
-                                      @RequestParam(value="oldPassword", defaultValue = "") String oldPassword){
+    public boolean isOldPasswordValid(@RequestParam(value = "userId", defaultValue = "") String userId,
+            @RequestParam(value = "oldPassword", defaultValue = "") String oldPassword) {
         return userService.isOldPasswordValid(userId, oldPassword);
     }
 
-    //API to add a new User into the table
+    // API to add a new User into the table
     @PostMapping
     @RequestMapping(path = "/addNewUser", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void addNewUser(@RequestBody User newUser){
+    public void addNewUser(@RequestBody User newUser) {
         userService.addNewUser(newUser);
     }
 
-    //API to delete a User from the table
+    // API to delete a User from the table
     @DeleteMapping(path = "/deleteUser/{userId}")
-    public void deleteUser(@PathVariable("userId") String userId){
+    public void deleteUser(@PathVariable("userId") String userId) {
         userService.deleteUser(userId);
     }
 
-    //API to update name and email of a User from the table
+    // API to update name and email of a User from the table
     @PutMapping(path = "/updateUserDetails/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void updateUserDetails(@PathVariable("userId") String userId,
-                                     @RequestBody User user){
+            @RequestBody User user) {
         userService.updateUserDetails(userId, user);
     }
 
-    //Get all books marked under given user
+    // Get all books marked under given user
     @GetMapping(path = "/getUserBooks/{userId}/")
     public Page<Book> getUsersBooks(@PathVariable("userId") String userId,
-                                                    @RequestParam(value="page", defaultValue = "0") int page,
-                                                    @RequestParam(value ="size", defaultValue = "10") int size){
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
         return userService.getUsersBooks(userId, page, size);
     }
 
     @PostMapping
     @RequestMapping(path = "/addBookToUser/{userId}/{newBookId}")
     public void addBookToUserByBookId(@PathVariable("userId") String userId,
-                                      @PathVariable("newBookId") String newBookId){
+            @PathVariable("newBookId") String newBookId) {
         userService.addBookToUserByBookId(userId, newBookId);
     }
 
     @PostMapping
     @RequestMapping(path = "/addBookToUser/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void addBookToUserByBookObject(@PathVariable("userId") String userId,
-                              @RequestBody Book newBook){
+            @RequestBody Book newBook) {
         userService.addBookToUserByBookObject(userId, newBook);
     }
 
     @DeleteMapping
     @RequestMapping(path = "/deleteBookFromUser/{userId}/{bookId}")
     public void deleteBookFromUser(@PathVariable("userId") String userId,
-                                   @PathVariable("bookId") String bookId){
+            @PathVariable("bookId") String bookId) {
         userService.deleteBookFromUser(userId, bookId);
     }
 
     @GetMapping
     @RequestMapping(path = "/searchBooks/{bookTitle}")
-    public List<Book> searchBooks(@PathVariable("bookTitle") String bookTitle){
+    public List<Book> searchBooks(@PathVariable("bookTitle") String bookTitle) {
         return userService.searchBooks(bookTitle);
     }
 
     @GetMapping
     @RequestMapping(path = "/isBookExistUnderUser/{userId}/{bookId}")
     public boolean isBookExistUnderUser(@PathVariable("userId") String userId,
-                                        @PathVariable("bookId") String bookId){
+            @PathVariable("bookId") String bookId) {
         return userService.isBookExistUnderUser(userId, bookId);
     }
 }
--- server/src/main/java/com/myappliction/springboot_application/user/UserService.java
+++ server/src/main/java/com/myappliction/springboot_application/user/UserService.java
@@ -17,11 +17,12 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Service
 public class UserService {
-    private static Logger log;
+    private static final Logger logger = LogManager.getLogger(UserController.class);
     private final UserRepository userRepository;
     private final BookService bookService;
 
@@ -31,108 +32,123 @@ public class UserService {
         this.bookService = bookService;
     }
 
-    //Gets all the Users in the Database
-    public List<User> getAllUsers(){
+    // Gets all the Users in the Database
+    public List<User> getAllUsers() {
+        logger.debug("Get ALL users API triggered");
         return userRepository.findAll();
     }
 
-    //Returns True if the given email exists in user table of database
-    public boolean isUserByEmailExist(String email){
-        //if there is any value in the result of JPQL Query then it means the email is already been registered by someone
+    // Returns True if the given email exists in user table of database
+    public boolean isUserByEmailExist(String email) {
+        // if there is any value in the result of JPQL Query then it means the email is
+        // already been registered by someone
+        logger.debug("Validating email already exist's API triggered");
         return userRepository.findUserByEmail(email).isPresent();
     }
 
-    //Returns the User by the given userId
-    public User getUser(String userId){
+    // Returns the User by the given userId
+    public User getUser(String userId) {
+        logger.debug("Get User by ID API triggered");
         return userRepository.findById(UUID.fromString(userId))
                 .orElseThrow(() -> new NoSuchUserExistsException("No User found by the given ID"));
     }
 
-    //Add the given User in the Database
-    public void addNewUser(User newUser){
-        if(isUserByEmailExist(newUser.getEmail())){
+    // Add the given User in the Database
+    public void addNewUser(User newUser) {
+        logger.debug("Adding New User API triggered");
+        if (isUserByEmailExist(newUser.getEmail())) {
             throw new UserAlreadyExistsException("Email is Taken");
-        }
-        else {
+        } else {
             userRepository.save(newUser);
         }
     }
 
-    //Delete the given User in Database by userId
-    public void deleteUser(String userId){
-        if(userRepository.existsById(UUID.fromString(userId)))
+    // Delete the given User in Database by userId
+    public void deleteUser(String userId) {
+        logger.debug("Delete a User API triggered");
+        if (userRepository.existsById(UUID.fromString(userId)))
             userRepository.deleteById(UUID.fromString(userId));
         else
             throw new NoSuchUserExistsException("No User found by the given ID");
     }
 
-    //Update the details of given user in Database
-    public void updateUserDetails(String userId, User updateUser){
+    // Update the details of given user in Database
+    public void updateUserDetails(String userId, User updateUser) {
+        logger.debug("Update User Details API triggered");
         User userById = getUser(userId);
 
-        if(updateUser.getFullName() != null &&
-                !updateUser.getFullName() .isEmpty() &&
+        if (updateUser.getFullName() != null &&
+                !updateUser.getFullName().isEmpty() &&
                 !Objects.equals(userById.getFullName(), updateUser.getFullName())) {
-            userById.setFullName(updateUser.getFullName() );
+            userById.setFullName(updateUser.getFullName());
         }
-        if(updateUser.getUserName() != null &&
-                !updateUser.getUserName() .isEmpty() &&
-                !Objects.equals(userById.getUserName(), updateUser.getUserName() )) {
-            userById.setUserName(updateUser.getUserName() );
+        if (updateUser.getUserName() != null &&
+                !updateUser.getUserName().isEmpty() &&
+                !Objects.equals(userById.getUserName(), updateUser.getUserName())) {
+            userById.setUserName(updateUser.getUserName());
         }
-        if(updateUser.getPassword() != null &&
-                !updateUser.getPassword() .isEmpty() &&
-                !Objects.equals(userById.getPassword(), updateUser.getPassword() )) {
-            userById.setPassword(updateUser.getPassword() );
+        if (updateUser.getPassword() != null &&
+                !updateUser.getPassword().isEmpty() &&
+                !Objects.equals(userById.getPassword(), updateUser.getPassword())) {
+            userById.setPassword(updateUser.getPassword());
         }
         userRepository.save(userById);
     }
 
-    //Validates the given password and user email and returns the user if exists by the given credentials
-    public String validateUserCred(String email, String password){
+    // Validates the given password and user email and returns the user if exists by
+    // the given credentials
+    public String validateUserCred(String email, String password) {
+        logger.debug("Validate User Credentials API triggered");
         User userByMail = userRepository.findUserByEmail(email)
                 .orElseThrow(() -> new NoSuchUserExistsException("No User found by the given Email"));
-        if(userByMail.getPassword().equals(password))
+        if (userByMail.getPassword().equals(password))
             return userByMail.getId().toString();
         else
             return "";
     }
 
-    //Returns True if the given username exists in user table of database
+    // Returns True if the given username exists in user table of database
     public boolean isUserByUsernameExist(String username) {
+        logger.debug("Validate user name already exits API triggered");
         return userRepository.findUserByUsername(username).isPresent();
     }
 
-    //Returns True if the given old password is valid for the given user
+    // Returns True if the given old password is valid for the given user
     public boolean isOldPasswordValid(String userId, String oldPassword) {
+        logger.debug("Validating Old password API triggered");
         User user = getUser(userId);
         return user.getPassword().equals(oldPassword);
     }
 
-    //Gets the Books under the given User
-    public Page<Book> getUsersBooks(String userId, int page, int size){
+    // Gets the Books under the given User
+    public Page<Book> getUsersBooks(String userId, int page, int size) {
+        logger.debug("Fetch user's books API triggered");
         return getUser(userId).getBooksList(PageRequest.of(page, size, Sort.by("name")));
     }
 
-    //Adds the given book by the given ID to the given user by userId
-    public void addBookToUserByBookId(String userId, String newBookId){
+    // Adds the given book by the given ID to the given user by userId
+    public void addBookToUserByBookId(String userId, String newBookId) {
+        logger.debug("Add a Book to User by their IDs API triggered");
         Book newBookById = bookService.getBook(UUID.fromString(newBookId));
         User userByID = getUser(userId);
         userByID.getBooksList().add(newBookById);
         userRepository.save(userByID);
     }
 
-    //Adds the given book to the given user by userId
-    public void addBookToUserByBookObject(String userId, Book newBook){
-        //First add the book in the book table and then add the book in the user's book list
+    // Adds the given book to the given user by userId
+    public void addBookToUserByBookObject(String userId, Book newBook) {
+        logger.debug("Add Book to user by new Book object API triggered");
+        // First add the book in the book table and then add the book in the user's book
+        // list
         bookService.addBook(newBook);
         User userByID = getUser(userId);
         userByID.getBooksList().add(newBook);
         userRepository.save(userByID);
     }
 
-    //Deletes the given book under given user by userId
+    // Deletes the given book under given user by userId
     public void deleteBookFromUser(String userId, String bookId) {
+        logger.debug("Delete a User's book API triggered");
         User userByID = getUser(userId);
         Book bookById = bookService.getBook(UUID.fromString(bookId));
         userByID.getBooksList().remove(bookById);
@@ -140,10 +156,12 @@ public class UserService {
     }
 
     public List<Book> searchBooks(String bookTitle) {
+        logger.debug("Search all books by title API triggered");
         return bookService.findBooksByTitle(bookTitle);
     }
 
     public boolean isBookExistUnderUser(String userId, String bookId) {
+        logger.debug("Validate Book already added under given User API triggered");
         Book bookById = bookService.getBook(UUID.fromString(bookId));
         return getUser(userId).getBooksList().contains(bookById);
     }
